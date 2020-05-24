aa67b2de09d2e0ef6590701cca7e02c6e710442a
Veldurty Ashwin <venkatashwinv@gmail.com>
2020-05-24 02:02:15 -0700

    Initial commit.

---
+++ DatingApp.Api/Migrations/20200523213403_IntinialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.Api.Migrations
+{
+    public partial class IntinialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Values",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Values", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Values");
+        }
+    }
+}
---
+++ DatingApp.Api/Models/Value.cs
@@ -0,0 +1,9 @@
+namespace DatingApp.Api.Models
+{
+    public class Value
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
